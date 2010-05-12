45a8775ea7c204c0247250fdb690e335d66fbfe6
aerivarola <aerivarola@fd862a59-2eb3-aadd-fb3b-5caba34f867a>
2010-05-12 20:31:39 +0000

    Se agrego en models.py a la clase artefacto algunos campos faltantes, se agregaron metodos unicode para la correcta representacion de los nombres. En settings se cambio el media_root.

--- app/models.py
+++ app/models.py
@@ -20,6 +20,9 @@ class Proyecto(models.Model):
     fecha_inicio = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
     fecha_fin = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
     cronograma = models.FileField(upload_to='cronogramas', null=True, blank=True)
+    
+    def __unicode__(self):
+        return self.nombre
 
 class Rol(models.Model):
     nombre = models.CharField(unique=True, max_length=50)
@@ -27,19 +30,28 @@ class Rol(models.Model):
     descripcion = models.TextField(null=True, blank=True)
     fecHor_creacion = models.DateTimeField(auto_now=False, auto_now_add=True, null=True, blank=True, editable=False)
     usuario_creador = models.ForeignKey(User)
+    
+    def __unicode__(self):
+        return self.nombre
 
 class Privilegio(models.Model):
     descripcion = models.TextField(null=True, blank=True)
 
 class Vista(models.Model):
     descripcion = models.TextField(null=True, blank=True)
-    
+
+"""
+Esta clase representa la fase del proyecto
+"""    
 class Fase(models.Model):
     nombre = models.CharField(unique=True, max_length=50)
     
     def __unicode__(self):
         return self.nombre
 
+""""
+Esta clase representa a que tipo pertenece un artefacto
+"""
 class TipoArtefacto(models.Model):
     nombre = models.CharField(unique=True, max_length=50)
     descripcion = models.TextField(null=True, blank=True)
@@ -48,10 +60,14 @@ class TipoArtefacto(models.Model):
     
     def __unicode__(self):
         return self.nombre
-
+"""
+Clase que representa a los artefactos
+"""
 class Artefacto(models.Model):   	
     nombre = models.CharField(max_length=50)
+    usuario = models.ForeignKey(User)
     estado = models.IntegerField(max_length=1, choices=STATUS_CHOICES, default=1)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
     version = models.PositiveIntegerField()#ojo
     complejidad = models.PositiveIntegerField()
     descripcion_corta = models.TextField(null=True, blank=True)
@@ -61,15 +77,21 @@ class Artefacto(models.Model):
     #relaciones con otras tablas
     relacionados = models.ManyToManyField("self")
     #claves foraneas
-    proyectos = models.ForeignKey(Proyecto)
+    proyecto = models.ForeignKey(Proyecto)
     tipo = models.ForeignKey(TipoArtefacto)
-   
+    
+    def __unicode__(self):
+        return self.nombre
+
 class Adjunto(models.Model):
     nombre = models.CharField(max_length=50)
-    contenido = models.FileField(upload_to='/archivos')
+    archivo = models.FileField(upload_to='/adjuntos')
     descripcion = models.TextField(null=True, blank=True)  
     #claves foraneas
     artefacto = models.ForeignKey(Artefacto)
+    
+    def __unicode__(self):
+        return self.nombre
 
 class LineaBase(models.Model):
     fechaCreacion = models.DateField(auto_now=False, auto_now_add=True, editable=False)
@@ -105,5 +127,3 @@ class UsuarioRolProyecto(models.Model):
 
     class Meta:
         unique_together = [("usuario", "rol", "proyecto")]
-   
-
--- urls.py
+++ urls.py
@@ -38,6 +38,8 @@ urlpatterns = patterns('',
     (r'^tipo_artefacto/mod/(?P<object_id>\d+)/$', update_object, {'form_class':TipoArtefactoForm, 'template_name':'proyecto/artefacto/abm_tipo_artefacto.html', 'post_save_redirect':'/tipo_artefacto', 'login_required':True}),
     (r'^tipo_artefacto/del/(?P<object_id>\d+)/$', delete_object, {'model':TipoArtefacto, 'template_name':'proyecto/artefacto/tipo_artefacto_confirm_delete.html', 'post_delete_redirect':'/tipo_artefacto', 'login_required':True}),
     
+    
+    
     (r'^terminar/$', terminar),
     (r'^logout/$', logout_pagina),
     
