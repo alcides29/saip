369fffacd5c01430b3acfee9511817db4257cccc
aerivarola <aerivarola@fd862a59-2eb3-aadd-fb3b-5caba34f867a>
2010-05-12 17:04:30 +0000

    Agragado abm proyecto, usuario, tipo artefactos, etc

---
+++ trunk/app/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from django.http import Http404
+from saip.app.forms import *
+#from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import logout
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+
+def principal(request):
+    return render_to_response('main_page.html', RequestContext(request))
+
+def lista(request, tipo):
+    user = User.objects.get(username=request.user.username)
+    if tipo == 'usuarios':
+        lista = User.objects.all()
+    elif tipo == 'proyectos':
+        lista = Proyecto.objects.all()
+    elif tipo == 'tipo_artefactos':    
+        lista = TipoArtefactos.objects.all()
+    else:
+		return render_to_response('error.html');
+    return render_to_response('lista.html',{'lista':lista, 'user':user, 'tipo':tipo})
+
+@login_required
+def admin_usuarios(request):
+	user = User.objects.get(username=request.user.username)
+	lista = User.objects.all()
+	return render_to_response('admin/usuarios/usuarios.html',{'lista':lista, 'user':user})
+
+@login_required
+def admin_proyectos(request):
+	user = User.objects.get(username=request.user.username)
+	lista = Proyecto.objects.all()
+	return render_to_response('admin/proyectos/proyectos.html',{'lista':lista, 'user':user})	
+
+@login_required
+def proyectos(request):
+	user = User.objects.get(username=request.user.username)
+	if request.method == 'POST':
+		form = ProyectosForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/terminar')
+	else:
+		form = ProyectosForm()
+	return render_to_response('admin/proyectos/abm_proyecto.html',{'form':form, 'user':user})
+
+# Administracion de tipo de artefactos
+@login_required
+def admin_tipo_artefacto(request):
+    user = User.objects.get(username=request.user.username)
+    lista = TipoArtefacto.objects.all()
+    return render_to_response('proyecto/artefacto/tipo_artefacto.html', {'lista': lista, 'user':user})
+'''
+def tipo_artefacto(request):
+    user = User.objects.get(username=request.user.username)
+    if request.method == 'POST':
+        form = TipoArtefactoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/terminar')
+    else:
+        form = ProyectosForm()
+    return render_to_response('proyecto/artefacto/abm_tipo_artefacto.html',{'form':form, 'user':user})
+'''
+
+@login_required
+def terminar(peticion):
+	return render_to_response('operacion_exitosa.html');
+
+def logout_pagina(request):
+    logout(request)
+    return render_to_response('logout.html')
+
+def login_redirect(request):
+	return HttpResponseRedirect('/login')
---
+++ trunk/urls.py
@@ -0,0 +1,47 @@
+from django.conf.urls.defaults import *
+from django.views.generic.create_update import *
+from django.contrib.auth.models import User
+
+import os.path
+
+from saip.app.forms import *
+from saip.app.models import *
+from saip.app.views import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^saip/', include('saip.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    (r'^$', principal),
+    (r'^login/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/login/$', login_redirect),
+    
+    # usuarios
+	(r'^usuarios/$', admin_usuarios),
+    (r'^usuarios/crear/$', create_object, {'form_class':UsuariosForm,'template_name':'admin/usuarios/abm_usuario.html', 'post_save_redirect':'/usuarios', 'login_required':True}),
+    (r'^usuarios/mod/(?P<object_id>\d+)/$', update_object, {'form_class':UsuariosForm, 'template_name':'admin/usuarios/abm_usuario.html', 'post_save_redirect':'/usuarios', 'login_required':True}),
+	(r'^usuarios/del/(?P<object_id>\d+)/$', delete_object, {'model':User, 'template_name':'admin/usuarios/user_confirm_delete.html', 'post_delete_redirect':'/usuarios', 'login_required':True}),
+    
+    # proyectos
+	(r'^proyectos/$', admin_proyectos),
+    (r'^proyectos/crear/$', create_object, {'form_class': ProyectosForm, 'template_name':'admin/proyectos/abm_proyecto.html', 'post_save_redirect':'/proyectos', 'login_required':True}),
+	(r'^proyectos/mod/(?P<object_id>\d+)/$', update_object, {'form_class':ProyectosForm, 'template_name':'admin/proyectos/abm_proyecto.html', 'post_save_redirect':'/proyectos', 'login_required':True}),
+	(r'^proyectos/del/(?P<object_id>\d+)/$', delete_object, {'model':Proyecto, 'template_name':'admin/proyectos/proyecto_confirm_delete.html', 'post_delete_redirect':'/proyectos', 'login_required':True}),
+    
+    # Tipo de artefacto
+    (r'^tipo_artefacto/$', admin_tipo_artefacto),
+    (r'^tipo_artefacto/crear/$', create_object, {'form_class': TipoArtefactoForm, 'template_name':'proyecto/artefacto/abm_tipo_artefacto.html', 'post_save_redirect':'/tipo_artefacto', 'login_required':True}),
+    (r'^tipo_artefacto/mod/(?P<object_id>\d+)/$', update_object, {'form_class':TipoArtefactoForm, 'template_name':'proyecto/artefacto/abm_tipo_artefacto.html', 'post_save_redirect':'/tipo_artefacto', 'login_required':True}),
+    (r'^tipo_artefacto/del/(?P<object_id>\d+)/$', delete_object, {'model':TipoArtefacto, 'template_name':'proyecto/artefacto/tipo_artefacto_confirm_delete.html', 'post_delete_redirect':'/tipo_artefacto', 'login_required':True}),
+    
+    (r'^terminar/$', terminar),
+    (r'^logout/$', logout_pagina),
+    
+    (r'^lista/(?P<tipo>\w+)/$', lista),
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+       {'document_root': os.path.abspath('site_media')}),
+)
