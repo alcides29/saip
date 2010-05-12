7cefe6b5ed4ddabcc59e6717614426fc71a71b5b
aerivarola <aerivarola@fd862a59-2eb3-aadd-fb3b-5caba34f867a>
2010-05-12 17:04:30 +0000

    Agragado abm proyecto, usuario, tipo artefactos, etc

---
+++ app/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+from django.contrib.auth.models import User
+from saip.app.models import *
+import datetime
+
+class UsuariosForm(forms.ModelForm):
+	username = forms.CharField(max_length=30, label='Usuario')
+	first_name = forms.CharField(max_length=30, label='Nombre')
+	last_name = forms.CharField(max_length=30, label='Apellido')
+	email = forms.EmailField(max_length=75, label='Correo Electronico')
+	password = forms.CharField(max_length=128, label='Contrasena', widget=forms.PasswordInput())
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'last_name', 'email', 'password')
+
+class ProyectosForm(forms.ModelForm):
+    nombre = forms.CharField(max_length=50, label='Nombre')
+    usuario_lider = forms.ModelChoiceField(queryset=User.objects.all(), label='Lider')
+    descripcion = forms.CharField(widget=forms.Textarea(), required=False, label='Descripcion')
+    fecha_inicio = forms.DateField(required=False, label='Fecha de Inicio')
+    fecha_fin = forms.DateField(required=False, label='Fecha de Finalizacion')
+    cronograma = forms.FileField(required=False, label='Cronograma')
+    class Meta:
+		model = Proyecto		
+		
+class RolesForm(forms.ModelForm):
+    nombre = forms.CharField(max_length=50, label='Nombre')
+    categoria = forms.CharField(max_length=1, widget=forms.Select(choices=CATEGORY_CHOICES), label='Categoria')
+    descripcion = forms.CharField(widget=forms.Textarea(), required=False, label='Descripcion')
+    fecHor_creacion = forms.DateTimeField(initial=datetime.datetime.now(), required=False, label='Fecha/Hora de creacion')#ojo
+    usuario_creador = forms.ModelChoiceField(queryset=User.objects.all(), label='Creador')
+    class Meta:
+    	model = Rol
+        
+class TipoArtefactoForm(forms.ModelForm):
+    nombre = forms.CharField(max_length=50, label='Nombre')
+    descripcion = forms.CharField(widget=forms.Textarea(), required=False, label='Descripcion')
+    fase = forms.ModelChoiceField(queryset=Fase.objects.all(), label='Fase')
+    class Meta:
+    	model = TipoArtefacto
+
+class ArtefactoForm(forms.ModelForm):   	
+    nombre = forms.CharField(max_length=50, label='Nombre')
+    estado = forms.CharField(max_length=1, widget=forms.Select(choices=STATUS_CHOICES), label='Estado')
+    version = forms.IntegerField() # No deberia estar en el form para editar
+    complejidad = forms.IntegerField()
+    descripcion_corta = forms.CharField(widget=forms.Textarea(), required=False, label='Descripcion Corta')
+    descripcion_larga = forms.CharField(widget=forms.Textarea(), required=False, label='Descripcion Larga')
+    habilitado = forms.BooleanField()
+    icono = forms.FileField(required=False, label='Icono de Artefacto')
+    relacionados = forms.ModelMultipleChoiceField(queryset=Artefacto.objects.all(), required=False, label='Artefactos relacionados')
+    proyectos = forms.ModelChoiceField(queryset=Proyecto.objects.all(), label='Proyectos')
+    tipo = forms.ModelChoiceField(queryset=TipoArtefacto.objects.all(), label='Tipo')
+    class Meta:
+        model = Artefacto
+        
---
+++ app/models.py
@@ -0,0 +1,109 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+CATEGORY_CHOICES = (
+ 	            ('1', 'Rol de Sistema'),
+ 	            ('2', 'Rol de Proyecto'),
+ 	        )    
+
+STATUS_CHOICES = (
+ 	            ('1', 'Pendiente'),
+ 	            ('2', 'Modificado'),
+                ('3', 'Revisado'),
+ 	        )
+
+class Proyecto(models.Model):
+    nombre = models.CharField(unique=True, max_length=50)
+    usuario_lider = models.ForeignKey(User)
+    descripcion = models.TextField(null=True, blank=True)
+    fecha_inicio = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    fecha_fin = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    cronograma = models.FileField(upload_to='cronogramas', null=True, blank=True)
+
+class Rol(models.Model):
+    nombre = models.CharField(unique=True, max_length=50)
+    categoria = models.IntegerField(max_length=1, choices=CATEGORY_CHOICES)
+    descripcion = models.TextField(null=True, blank=True)
+    fecHor_creacion = models.DateTimeField(auto_now=False, auto_now_add=True, null=True, blank=True, editable=False)
+    usuario_creador = models.ForeignKey(User)
+
+class Privilegio(models.Model):
+    descripcion = models.TextField(null=True, blank=True)
+
+class Vista(models.Model):
+    descripcion = models.TextField(null=True, blank=True)
+    
+class Fase(models.Model):
+    nombre = models.CharField(unique=True, max_length=50)
+    
+    def __unicode__(self):
+        return self.nombre
+
+class TipoArtefacto(models.Model):
+    nombre = models.CharField(unique=True, max_length=50)
+    descripcion = models.TextField(null=True, blank=True)
+    #claves foraneas
+    fase = models.ForeignKey(Fase)
+    
+    def __unicode__(self):
+        return self.nombre
+
+class Artefacto(models.Model):   	
+    nombre = models.CharField(max_length=50)
+    estado = models.IntegerField(max_length=1, choices=STATUS_CHOICES, default=1)
+    version = models.PositiveIntegerField()#ojo
+    complejidad = models.PositiveIntegerField()
+    descripcion_corta = models.TextField(null=True, blank=True)
+    descripcion_larga = models.TextField(null=True, blank=True)
+    habilitado = models.BooleanField()
+    icono = models.FileField(upload_to='/icono', null=True, blank=True)
+    #relaciones con otras tablas
+    relacionados = models.ManyToManyField("self")
+    #claves foraneas
+    proyectos = models.ForeignKey(Proyecto)
+    tipo = models.ForeignKey(TipoArtefacto)
+   
+class Adjunto(models.Model):
+    nombre = models.CharField(max_length=50)
+    contenido = models.FileField(upload_to='/archivos')
+    descripcion = models.TextField(null=True, blank=True)  
+    #claves foraneas
+    artefacto = models.ForeignKey(Artefacto)
+
+class LineaBase(models.Model):
+    fechaCreacion = models.DateField(auto_now=False, auto_now_add=True, editable=False)
+    #relaciones con otras tablas
+    proyectos = models.ForeignKey(Proyecto)
+    Fase = models.OneToOneField(Fase, parent_link=False)#ver
+
+class Historial(models.Model):
+    fechaCreacion = models.DateField(auto_now =False, auto_now_add=True, editable=False)
+    #claves foraneas
+    artefacto = models.OneToOneField(Artefacto, parent_link=False)#ojo
+
+class RegitroHistorial(models.Model):
+    version = models.PositiveIntegerField()#ojo
+    descripcion = models.TextField(null=True, blank=True)
+    fecha_modificacion = models.DateField(auto_now=True, auto_now_add=False)#ojo
+    #claves foraneas
+    historial = models.ForeignKey(Historial)
+
+class Permiso(models.Model):
+    #claves foraneas
+    rol = models.ForeignKey(Rol)
+    privilegio = models.ForeignKey(Privilegio)
+    vista = models.ForeignKey(Vista)
+
+    class Meta:
+        unique_together = [("rol", "privilegio", "vista")]
+
+class UsuarioRolProyecto(models.Model):   
+    usuario = models.ForeignKey(User)
+    rol = models.ForeignKey(Rol)
+    proyecto = models.ForeignKey(Proyecto)
+
+    class Meta:
+        unique_together = [("usuario", "rol", "proyecto")]
+   
+
