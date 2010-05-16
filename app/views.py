9b3740c1c35b437270aa3904b5e7187263cf1058
aerivarola <aerivarola@fd862a59-2eb3-aadd-fb3b-5caba34f867a>
2010-05-16 14:08:07 +0000

    Se agrega los metodos que no se copiaron correctamente (add_user, admin_roles y crear_rol) en view. También se cambió la posición de los comentarios

--- app/views.py
+++ app/views.py
@@ -1,14 +1,17 @@
+import hashlib
+
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.http import Http404
-from saip.app.forms import *
-#from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth import logout
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 
+from saip.app.forms import *
+
 def principal(request):
+    """Muestra la pagina principal."""
     return render_to_response('main_page.html', RequestContext(request))
 
 @login_required
@@ -31,7 +34,9 @@ def admin_usuarios_proyecto(request, object_id):
 			lista.append(nuevo)
 		oform = ItemForm(miembros[0].usuario, initial={'item': '2'})
 	return render_to_response('admin/proyectos/admin_miembros.html',{'otroform':oform, 'lista':lista, 'user':user, 'proyecto':Proyecto.objects.get(id=object_id)})
-	'''p = Proyecto.objects.get(pk = object_id)
+    
+'''
+    p = Proyecto.objects.get(pk = object_id)
 	miembros = UsuarioRolProyecto.objects.filter(proyecto = p)
 	print miembros
 	if request.method == 'POST':
@@ -44,7 +49,8 @@ def admin_usuarios_proyecto(request, object_id):
 			return HttpResponseRedirect("/proyectos")
 	else:
 		form = MiembrosProyectoForm(miembros)
-	return render_to_response('admin/proyectos/admin_miembros.html',{'form':form, 'user':user, 'proyecto':Proyecto.objects.get(id=object_id)})	'''
+	return render_to_response('admin/proyectos/admin_miembros.html',{'form':form, 'user':user, 'proyecto':Proyecto.objects.get(id=object_id)})
+'''
 		
 @login_required
 def add_usuario_proyecto(request, object_id):
@@ -62,31 +68,32 @@ def add_usuario_proyecto(request, object_id):
 		form = UsuarioProyectoForm()
 	return render_to_response('admin/proyectos/add_miembro.html', {'form':form, 'user':user})
 
-"""Agrega un nuevo usuario"""
 @login_required
 def add_user(request):
-	user = User.objects.get(username=request.user.username)
-	if request.method == 'POST':
-		form = UsuariosForm(request.POST)
-		if form.is_valid():
-			nuevo = User()
-			nuevo.username = form.cleaned_data['username']
-			nuevo.first_name = form.cleaned_data['first_name']
-			nuevo.last_name = form.cleaned_data['last_name']
-			nuevo.email = form.cleaned_data['email']
-			nuevo.set_password(form.cleaned_data['password'])
-			nuevo.is_staff = True
-			nuevo.is_active = True
-			nuevo.is_superuser = True #no se si esta bien este
-			nuevo.last_login = datetime.datetime.now()
-			nuevo.date_joined = datetime.datetime.now()
-			nuevo.save()
-			return HttpResponseRedirect("/usuarios")
-	else:
-		form = UsuariosForm()
-	return render_to_response('admin/usuario
+    """Agrega un nuevo usuario."""
+    user = User.objects.get(username=request.user.username)
+    if request.method == 'POST':
+        form = UsuariosForm(request.POST)
+        if form.is_valid():
+            nuevo = User()
+            nuevo.username = form.cleaned_data['username']
+            nuevo.first_name = form.cleaned_data['first_name']
+            nuevo.last_name = form.cleaned_data['last_name']
+            nuevo.email = form.cleaned_data['email']
+            nuevo.set_password(form.cleaned_data['password'])
+            nuevo.is_staff = True
+            nuevo.is_active = True
+            nuevo.is_superuser = True #no se si esta bien este
+            nuevo.last_login = datetime.datetime.now()
+            nuevo.date_joined = datetime.datetime.now()
+            nuevo.save()
+            return HttpResponseRedirect("/usuarios")
+    else:
+        form = UsuariosForm()
+        return render_to_response('admin/usuarios/abm_usuario.html',{'form':form, 'user':user})
 
 def lista(request, tipo):
+    """Metodo de prueba para listar items"""
     user = User.objects.get(username=request.user.username)
     if tipo == 'usuarios':
         lista = User.objects.all()
@@ -100,15 +107,43 @@ def lista(request, tipo):
 
 @login_required
 def admin_usuarios(request):
-	user = User.objects.get(username=request.user.username)
-	lista = User.objects.all()
-	return render_to_response('admin/usuarios/usuarios.html',{'lista':lista, 'user':user})
+    """Administracion general de usuarios"""
+    user = User.objects.get(username=request.user.username)
+    lista = User.objects.all()
+    return render_to_response('admin/usuarios/usuarios.html',{'lista':lista, 'user':user})
 
 @login_required
 def admin_proyectos(request):
-	user = User.objects.get(username=request.user.username)
-	lista = Proyecto.objects.all()
-	return render_to_response('admin/proyectos/proyectos.html',{'lista':lista, 'user':user})	
+    """Administracion general de proyectos"""
+    user = User.objects.get(username=request.user.username)
+    lista = Proyecto.objects.all()
+    return render_to_response('admin/proyectos/proyectos.html',{'lista':lista, 'user':user})	
+
+@login_required
+def admin_roles(request):
+    """Administracion general de roles"""
+    user = User.objects.get(username=request.user.username)
+    lista = Rol.objects.all()
+    return render_to_response('admin/roles/roles.html',{'lista':lista, 'user':user})
+
+@login_required
+def crear_rol(request):
+    """Agrega un nuevo rol."""
+    user = User.objects.get(username=request.user.username)
+    if request.method == 'POST':
+        form = RolesForm(request.POST)
+        if form.is_valid():
+            r = Rol()
+            r.nombre = form.cleaned_data['nombre']
+            r.categoria = form.cleaned_data['categoria']
+            r.descripcion = form.cleaned_data['descripcion']
+            r.fecHor_creacion = datetime.datetime.now()
+            r.usuario_creador = user
+            r.save()
+            return HttpResponseRedirect('/roles')
+    else:
+        form = RolesForm()
+    return render_to_response('admin/roles/abm_rol.html',{'form':form, 'user':user})
 
 @login_required
 def proyectos(request):
@@ -132,11 +167,14 @@ def admin_tipo_artefacto(request):
 
 @login_required
 def terminar(peticion):
-	return render_to_response('operacion_exitosa.html');
+    """Muestra una pagina de confirmacion de exito"""
+    return render_to_response('operacion_exitosa.html');
 
 def logout_pagina(request):
+    """Pagina de logout"""
     logout(request)
     return render_to_response('logout.html')
 
 def login_redirect(request):
-	return HttpResponseRedirect('/login')
+    """Redirige de /accounts/login a /login."""
+    return HttpResponseRedirect('/login')
