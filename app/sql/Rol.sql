09be9b235c4e859a5213dcdae47eb10dfd52700f
aerivarola <aerivarola@fd862a59-2eb3-aadd-fb3b-5caba34f867a>
2010-05-25 15:52:50 +0000

    Se arreglaron los scripts sql para la carga automatica al hacer syncdb.

--- app/sql/Rol.sql
+++
@@ -1,2 +0,0 @@
-﻿insert into app_rol(nombre, categoria, descripcion)
-values ('superusuario', 1, 'rol del superusuario'), ('lider de proyecto', 2, 'rol de lider de proyecto')
--- app/sql/UsuarioRolSistema.sql
+++
@@ -1,2 +0,0 @@
-insert into app_usuariorolsistema(usuario_id, rol_id)
-values (1,1)
