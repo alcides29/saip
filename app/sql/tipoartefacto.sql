520c4db958a6140d354aac82a106f15c9137bf8d
aerivarola <aerivarola@fd862a59-2eb3-aadd-fb3b-5caba34f867a>
2010-05-26 16:57:05 +0000

    script sql con tipo de artefactos iniciales

---
+++ app/sql/tipoartefacto.sql
@@ -0,0 +1,14 @@
+INSERT INTO app_tipoartefacto(nombre, descripcion, fase_id)
+    VALUES ('RF', 'Requerimiento Funcional', 1),
+    ('RNF', 'Requerimiento No Funcional', 1)
+    ('CUG', 'Casos de Uso General', 1),
+    ('DER', 'Diagrama de Entidad Relacion', 2),
+    ('DC', 'Diagrama de Clase', 2),
+    ('DS', 'Diagrama de Secuencia', 2),
+    ('DCO', 'Diagrama de Colaboracion', 2),
+    ('DA', 'Diagrama de Actividad', 2),
+    ('DE', 'Diagrama de Estado', 2),
+    ('DCM', 'Diagrama de Componente', 2),
+    ('DD', 'Diagrama de Despliegue', 2),
+    ('CO', 'Codigo', 3)
+    ('LI', 'Libreria', 3),;
