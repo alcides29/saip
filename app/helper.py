f6cc644ccfba5f6d667c093c16a28d92715eed9b
romifz <romifz@fd862a59-2eb3-aadd-fb3b-5caba34f867a>
2010-05-28 15:18:18 +0000

    Las relaciones entre artefactos y la version inicial de calcular el impacto... Falta todavia controlar algunas cosas; tambien cambie la pagina de cambiar contraseña

--- app/helper.py
+++ app/helper.py
@@ -1,12 +1,39 @@
+from saip.app.models import *
 
 def validar_fase(proyecto, fase):
+    print proyecto.fase.id
+    print fase
     if proyecto.fase.id == 1: 
-        if fase == 2 or fase ==3: return false;
+        if int(fase) == 2 or int(fase) == 3: return False;
         return True
     if proyecto.fase.id == 2:
-        if fase == 1 or fase == 2: return True
+        if int(fase) == 1 or int(fase) == 2: return True
         return False
     if proyecto.fase.id == 3:
-        if fase == 2 or fase == 3: return True
+        if int(fase) == 2 or int(fase) == 3: return True
         return False
     return False
+
+def obtener_relaciones_izq(art):
+    relaciones = RelArtefacto.objects.filter(hijo = art)
+    if relaciones:
+        ret =[]
+        for item in relaciones:
+            aux = obtener_relaciones_izq(item.padre)
+            if aux:
+                ret.extend(aux)
+        return ret
+    else: 
+        return [art]
+
+def obtener_relaciones_der(art):
+    relaciones = RelArtefacto.objects.filter(padre = art)
+    if relaciones:
+        ret =[]
+        for item in relaciones:
+            aux = obtener_relaciones_der(item.hijo)
+            if aux:
+                ret.extend(aux)
+        return ret
+    else: 
+        return [art]
