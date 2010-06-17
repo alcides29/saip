e4cef779ab5e29d3e97429b37e39b5b20dea8006
romifz <romifz@fd862a59-2eb3-aadd-fb3b-5caba34f867a>
2010-06-17 19:28:20 +0000

    Cambios en calculo de impacto y relaciones entre artefactos

---
+++ trunk/app/templatetags/app_extras.py
@@ -0,0 +1,13 @@
+﻿from django import template
+register = template.Library()
+
+@register.filter(name='display')
+
+def display(value):
+    if value == 1:
+        return 'Pendiente'
+    elif value == 2:
+        return 'Modificado'
+    elif value == 3:
+        return 'Revisado'
+    return ''
