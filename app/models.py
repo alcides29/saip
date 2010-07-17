f6a177ddc323119ca97d4da82d9f7a018cca89c9
romifz <romifz@fd862a59-2eb3-aadd-fb3b-5caba34f867a>
2010-07-17 04:02:24 +0000

    Comentarios en models.py

--- app/models.py
+++ app/models.py
@@ -28,6 +28,10 @@ STATUS_CHOICES = (
  	        )
 
 class Permiso(models.Model):
+    """
+    Representa una accion posible sobre una vista.
+    
+    """
     nombre = models.CharField(unique=True, max_length = 50)
     categoria = models.IntegerField(max_length=1, choices=CATEGORY_CHOICES)
     
@@ -35,6 +39,10 @@ class Permiso(models.Model):
         return self.nombre
 
 class Rol(models.Model):
+    """
+    Representa un conjunto de permisos que definen las tareas realizables por el usuario que lo posea.
+    
+    """
     nombre = models.CharField(unique=True, max_length=50)
     categoria = models.IntegerField(max_length=1, choices=CATEGORY_CHOICES)
     descripcion = models.TextField(null=True, blank=True)
@@ -46,19 +54,30 @@ class Rol(models.Model):
         return self.nombre
 
 class Fase(models.Model):
-    """Esta clase representa la fase del proyecto."""
+    """
+    Esta clase representa la fase en la que puede encontrarse un proyecto
+    
+    """
     nombre = models.CharField(unique=True, max_length=50)
     
     def __unicode__(self):
         return self.nombre
     
 class RolPermiso(models.Model):
+    """ 
+    Tabla asociativa entre Permiso y Rol. El campo fase define (si el rol es de proyecto) en que fase del proyecto el permiso
+    es valido.
+    
+    """
     rol = models.ForeignKey(Rol)
     permiso = models.ForeignKey(Permiso)
     fase = models.ForeignKey(Fase, null = True)
 
 class Proyecto(models.Model):
-    """Clase que representa un proyecto."""
+    """
+    Clase que representa un proyecto.
+    
+    """
     nombre = models.CharField(unique=True, max_length=50)
     usuario_lider = models.ForeignKey(User)
     fase = models.ForeignKey(Fase)
@@ -71,7 +90,10 @@ class Proyecto(models.Model):
         return self.nombre
     
 class TipoArtefacto(models.Model):
-    """"Esta clase representa a que tipo pertenece un artefacto."""
+    """
+    Esta clase representa el tipo al que pertenece un artefacto.
+    
+    """
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=50)
     #claves foraneas
@@ -82,7 +104,10 @@ class TipoArtefacto(models.Model):
         return self.nombre
 
 class TipoArtefactoFaseProyecto(models.Model):
-    """Tabla que relaciona el tipo de artefacto a una fase por proyecto."""
+    """
+    Tabla que relaciona el tipo de artefacto a una fase por proyecto.
+    
+    """
     proyecto = models.ForeignKey(Proyecto)
     fase = models.ForeignKey(Fase)
     tipo_artefacto = models.ForeignKey(TipoArtefacto)
@@ -95,7 +120,10 @@ class TipoArtefactoFaseProyecto(models.Model):
         unique_together = [("tipo_artefacto", "fase", "proyecto")]
 
 class Artefacto(models.Model):
-    """Clase que representa a los artefactos."""
+    """
+    Clase que representa a los artefactos.
+    
+    """
     nombre = models.CharField(max_length=50)
     usuario = models.ForeignKey(User)
     fecha_creacion = models.DateField(auto_now_add=True, editable = False)
@@ -114,6 +142,10 @@ class Artefacto(models.Model):
         return self.nombre
 
 class RelArtefacto(models.Model):
+    """
+    Tabla asociativa que representa la relacion entre dos artefactos.
+    
+    """
     padre = models.ForeignKey(Artefacto, related_name = 'padre')
     hijo = models.ForeignKey(Artefacto, related_name = 'hijo')
     habilitado = models.BooleanField(default=True)
@@ -122,14 +154,20 @@ class RelArtefacto(models.Model):
         unique_together = [("padre", "hijo")]
 
 class Historial(models.Model):
-    """Clase que representa el historial de los artefactos"""
+    """
+    Clase que representa el historial de los artefactos.
+    
+    """
     usuario = models.ForeignKey(User)
     fecha_creacion = models.DateField(auto_now =False, auto_now_add=True, editable=False)
     #claves foraneas
     artefacto = models.OneToOneField(Artefacto, parent_link=False)
     
 class RegistroHistorial(models.Model):
-    """Clase que representa el Registro de versiones de los artefactos"""
+    """
+    Clase que representa el registro de una version anterior de un artefacto.
+    
+    """
     version = models.PositiveIntegerField()
     complejidad = models.IntegerField()
     descripcion_corta = models.TextField(null=True, blank=True)
@@ -142,7 +180,10 @@ class RegistroHistorial(models.Model):
    
     
 class Adjunto(models.Model):
-    #archivo = models.FileField(upload_to='artefactos')
+    """
+    Almacena el contenido de un archivo adjunto a un artefacto y sus atributos.
+    
+    """
     nombre = models.CharField(max_length = 100)
     contenido = models.TextField(null=True)
     tamanho = models.IntegerField()
@@ -152,13 +193,21 @@ class Adjunto(models.Model):
     habilitado = models.BooleanField(default = True)
     
 class LineaBase(models.Model):
+    """
+    Almacena los datos de la generacion de una linea base.
+    
+    """
     fecha_creacion = models.DateField(auto_now=False, auto_now_add=True, editable=False)
     #relaciones con otras tablas
     proyectos = models.ForeignKey(Proyecto)
     fase = models.ForeignKey(Fase)
 
 
-class UsuarioRolProyecto(models.Model):   
+class UsuarioRolProyecto(models.Model):
+    """
+    Tabla asociativa que representa la relacion entre un usuario, su rol dentro de un proyecto y el proyecto mismo.
+    
+    """  
     usuario = models.ForeignKey(User)
     rol = models.ForeignKey(Rol, null=True)
     proyecto = models.ForeignKey(Proyecto)
@@ -167,6 +216,10 @@ class UsuarioRolProyecto(models.Model):
         unique_together = [("usuario", "rol", "proyecto")]
         
 class UsuarioRolSistema(models.Model):
+    """
+    Tabla asociativa que representa la relacion entre un usuario y un rol de sistema que posee.
+    
+    """
     usuario = models.ForeignKey(User)
     rol = models.ForeignKey(Rol)
     
@@ -174,6 +227,10 @@ class UsuarioRolSistema(models.Model):
         unique_together = [("usuario", "rol")]
         
 class RegHistoRel(models.Model):
+    """
+    Clase que almacena las relaciones de una version anterior de un artefacto.
+    
+    """
     art_padre = models.ForeignKey(Artefacto, related_name = 'art_padre')
     art_hijo = models.ForeignKey(Artefacto, related_name = 'art_hijo')
     registro = models.ForeignKey(RegistroHistorial)
@@ -182,6 +239,10 @@ class RegHistoRel(models.Model):
         unique_together = [("art_padre", "art_hijo", "registro")]
         
 class RegHistoAdj(models.Model):
+    """
+    Clase que almacena los archivos adjuntos de una version anterior de un artefacto.
+    
+    """
     #archivo = models.FileField(upload_to='artefactos')
     nombre = models.CharField(max_length = 100)
     contenido = models.TextField(null=True)
@@ -191,5 +252,4 @@ class RegHistoAdj(models.Model):
     artefacto = models.ForeignKey(Artefacto)
     #habilitado = models.BooleanField(default = True)
     registro = models.ForeignKey(RegistroHistorial)
-
 
